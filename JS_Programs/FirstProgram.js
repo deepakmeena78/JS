ec43a9044714bc9acced5511c0cfe7eaec07de9c
Deepak meena <deepakmeenaa78@gmail.com>
2024-10-06 00:59:57 +0530

    New Programs

---
+++ JS_Programs/FirstProgram.js
@@ -0,0 +1,5 @@
+function ph1(e) {
+    let x = 10;
+    return x + e;
+}
+console.log(ph1(7));
---
+++ SelfPractice/Project/4_Guess_a_Number/main.js
@@ -0,0 +1,38 @@
+const math = (parseInt(Math.random() * 100 + 1));
+const submit1 = document.querySelector('#sub')
+const userinput = document.querySelector('#guessField')
+const guesslot = document.querySelector('.guesses')
+const remain = document.querySelector('.lastResult')
+const low = document.querySelector('.low')
+const result = document.querySelector('.result')
+
+const p = document.createElement('p');
+let pregues = []
+let mygues = 1;
+
+let playgame = true;
+
+if(playgame){
+    submit1.addEventListener('click',function(e){
+        e.preventDefault()
+        const guess = parseInt(userinput.valie);
+    })
+}
+function validguess(guess) {
+
+}
+function check(guess) {
+
+}
+function display(guess) {
+
+}
+function massege(guess) {
+
+}
+function nawgame(guess) {
+
+}
+function endgame(guess) {
+
+}
