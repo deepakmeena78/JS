6bca9dcb884a533d99526f0557ab6d0348396ad1
Deepak meena <deepakmeenaa78@gmail.com>
2025-01-13 22:10:21 +0530

    New

--- JS_Programs/SelfPractice/Express/CRUD_5/index.js
+++ JS_Programs/SelfPractice/Express/CRUD_5/index.js
@@ -29,7 +29,7 @@ app.get("/edit/:id", async (req, res) => {
     if (!edituser) {
         res.redirect("/");
     } else {
-        res.render("edit", { edituser: edituser });
+        res.render("edit", { userDAta: edituser });
     }
 });
 
