abc2933b6e9fbe7d5070ea0652ba900f94c0f001
Deepak meena <deepakmeenaa78@gmail.com>
2025-01-16 09:31:38 +0530

    New Commit

---
+++ JS_Programs/SelfPractice/Express/JWT-Authentication/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const userSchema = new mongoose({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
